19707c2f884e915847be54c35cf8ccf06215691c
obzva <dngyng1000@gmail.com>
2023-05-05 16:46:59 +0900

    Finished Gh-pages settings

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,5 @@
 <script>
 	import ArticleList from '$lib/components/article/ArticleList.svelte';
-	import Footer from '$lib/components/layout/Footer.svelte';
-	import Header from '$lib/components/layout/Header.svelte';
 </script>
 
 <ArticleList />
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	base:"obzva.github.io",
+	base: '/obzva.github.io/',
 	plugins: [sveltekit()]
 });
